3f55df1bd541ffe41a52a87160acf42605b02a0c
hwungzxje <24520596@gm.uit.edu.vn>
2025-11-24 01:15:49 +0700

    update bai05

--- Lab04/Bai05_/Form1.Designer.cs
+++ Lab04/Bai05_/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +13,83 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lb_taikhoan = new System.Windows.Forms.Label();
+            this.tb_taikhoan = new System.Windows.Forms.TextBox();
+            this.lb_matkhau = new System.Windows.Forms.Label();
+            this.tb_matkhau = new System.Windows.Forms.TextBox();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.tb_ketqua = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+
+            this.lb_taikhoan.AutoSize = true;
+            this.lb_taikhoan.Location = new System.Drawing.Point(25, 25);
+            this.lb_taikhoan.Name = "lb_taikhoan";
+            this.lb_taikhoan.Size = new System.Drawing.Size(70, 15);
+            this.lb_taikhoan.TabIndex = 0;
+            this.lb_taikhoan.Text = "Tài khoản:";
+
+            this.tb_taikhoan.Location = new System.Drawing.Point(101, 22);
+            this.tb_taikhoan.Name = "tb_taikhoan";
+            this.tb_taikhoan.Size = new System.Drawing.Size(250, 21);
+            this.tb_taikhoan.TabIndex = 1;
+
+            this.lb_matkhau.AutoSize = true;
+            this.lb_matkhau.Location = new System.Drawing.Point(25, 60);
+            this.lb_matkhau.Name = "lb_matkhau";
+            this.lb_matkhau.Size = new System.Drawing.Size(64, 15);
+            this.lb_matkhau.TabIndex = 2;
+            this.lb_matkhau.Text = "Mật khẩu:";
+
+            this.tb_matkhau.Location = new System.Drawing.Point(101, 57);
+            this.tb_matkhau.Name = "tb_matkhau";
+            this.tb_matkhau.PasswordChar = '*';
+            this.tb_matkhau.Size = new System.Drawing.Size(250, 21);
+            this.tb_matkhau.TabIndex = 3;
+
+            this.btn_dangnhap.BackColor = System.Drawing.Color.SteelBlue;
+            this.btn_dangnhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_dangnhap.ForeColor = System.Drawing.Color.White;
+            this.btn_dangnhap.Location = new System.Drawing.Point(28, 95);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(323, 35);
+            this.btn_dangnhap.TabIndex = 4;
+            this.btn_dangnhap.Text = "ĐĂNG NHẬP";
+            this.btn_dangnhap.UseVisualStyleBackColor = false;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+
+            this.tb_ketqua.Location = new System.Drawing.Point(28, 145);
+            this.tb_ketqua.Name = "tb_ketqua";
+            this.tb_ketqua.ReadOnly = true;
+            this.tb_ketqua.Size = new System.Drawing.Size(323, 195);
+            this.tb_ketqua.TabIndex = 5;
+            this.tb_ketqua.Text = "Nhập thông tin và click ĐĂNG NHẬP...";
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.tb_ketqua);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.tb_matkhau);
+            this.Controls.Add(this.lb_matkhau);
+            this.Controls.Add(this.tb_taikhoan);
+            this.Controls.Add(this.lb_taikhoan);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bài 5 - Đăng nhập API";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        private System.Windows.Forms.Label lb_taikhoan;
+        private System.Windows.Forms.TextBox tb_taikhoan;
+        private System.Windows.Forms.Label lb_matkhau;
+        private System.Windows.Forms.TextBox tb_matkhau;
+        private System.Windows.Forms.Button btn_dangnhap;
+        private System.Windows.Forms.RichTextBox tb_ketqua;
     }
 }
-
--- Lab04/Bai05_/Form1.cs
+++ Lab04/Bai05_/Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace Bai05_
 {
@@ -15,6 +12,72 @@ namespace Bai05_
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void btn_dangnhap_Click(object sender, EventArgs e)
+        {
+            await DangNhapAPI(tb_taikhoan.Text.Trim(), tb_matkhau.Text);
+        }
+
+        private async Task DangNhapAPI(string username, string password)
+        {
+            string url = "https://nt106.uitiot.vn/auth/token";
+
+            btn_dangnhap.Enabled = false;
+            btn_dangnhap.Text = "ĐANG XỬ LÝ...";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("accept", "application/json");
+
+                    var formData = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("grant_type", ""),
+                        new KeyValuePair<string, string>("username", username),
+                        new KeyValuePair<string, string>("password", password),
+                        new KeyValuePair<string, string>("scope", ""),
+                        new KeyValuePair<string, string>("client_id", ""),
+                        new KeyValuePair<string, string>("client_secret", "")
+                    };
+
+                    var content = new FormUrlEncodedContent(formData);
+
+                    tb_ketqua.Text = $"Testing: {username}\r\n";
+                    tb_ketqua.Text += "Connecting to server...\r\n";
+
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+                    string responseString = await response.Content.ReadAsStringAsync();
+
+                    tb_ketqua.Text += $"Response Code: {(int)response.StatusCode}\r\n\r\n";
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        JObject responseObject = JObject.Parse(responseString);
+                        string tokenType = responseObject["token_type"].ToString();
+                        string accessToken = responseObject["access_token"].ToString();
+
+                        tb_ketqua.Text += $"{tokenType} {accessToken}\r\n";
+                        tb_ketqua.Text += "Đăng nhập thành công";
+                    }
+                    else
+                    {
+                        JObject responseObject = JObject.Parse(responseString);
+                        string detail = responseObject["detail"]?.ToString() ?? responseString;
+                        tb_ketqua.Text += $"Error: {detail}";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                tb_ketqua.Text += $"System Error: {ex.Message}";
+            }
+            finally
+            {
+                btn_dangnhap.Enabled = true;
+                btn_dangnhap.Text = "ĐĂNG NHẬP";
+            }
         }
     }
 }
